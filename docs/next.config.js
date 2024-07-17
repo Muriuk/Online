0fbe4a165debd9a8a47f3537814bf0019634043d
Kelvin Muriuki <105153920+Muriuk@users.noreply.github.com>
2024-07-17 15:36:54 +0300

    Add files via upload

---
+++ docs/next.config.js
@@ -0,0 +1,14 @@
+const env = process.env.NODE_ENV;
+
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx'
+});
+
+module.exports = {
+  basePath: env === 'production' ? '/docs' : undefined,
+  env: {
+    imagePath: env === 'production' ? '/docs' : ''
+  },
+  ...withNextra()
+};
